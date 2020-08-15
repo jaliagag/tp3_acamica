40eba8b0ac345da2009a52f01da5a94089663a11
Jose Aliaga <jmfaliaga@gmail.com>
2020-08-15 18:41:21 -0300

    desde mac starting

---
+++ tp3_1.0/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+// routes
+
+app.get('/delilah.resto.com.ar', (req, res) => {
+    res.json({
+        body: 'home'
+    })
+});
+
+app.get('/delilah.resto.com.ar/login', (req, res) => {
+    res.json({
+        body: 'login'
+    })
+});
+
+app.get('/delilah.resto.com.ar/:usr', (req, res) => {
+    res.json({
+        body: 'user_home'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000')
+})
